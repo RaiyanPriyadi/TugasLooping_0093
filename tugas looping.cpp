4b9656881fe3b63903e044f82db98284b80868cb
RaiyanPriyadi <dwi.raiyan.ft25@mail.umy.ac.id>
2026-04-01 09:40:22 +0700

    Update tugas looping.cpp

--- tugas looping.cpp
+++ tugas looping.cpp
@@ -8,7 +8,7 @@ int pilihan;
 // Fungsi (Prosedur) untuk menampilkan menu utama
 void tampilkanMenu() {
     cout << "\n======================================\n";
-    cout << "      MENU PENGECEKAN BILANG\n";
+    cout << "      MENU PENGECEKAN BILANGAN\n";
     cout << "======================================\n";
     cout << "1. Cek Bilangan Prima\n";
     cout << "2. Cek Bilangan Fibonacci\n";
